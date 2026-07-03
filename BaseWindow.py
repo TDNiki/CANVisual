be52fa4389cc0ce1160fd5ea9f648ae76bb95f45
Nikita <znikidi@gmail.com>
2026-07-03 18:46:54 +0500

    deleted no needed func

--- BaseWindow.py
+++ BaseWindow.py
@@ -11,10 +11,3 @@ class BaseWindow:
     @classmethod
     def update(cls):
         pass
-
-    @classmethod
-    def resize_window(cls, width, height) -> tuple[int, int, int, int]:
-        """params: The current size of the application window
-        \n :RETURNS: updated window child sizes
-        """
-        return (int(width * cls.size[0]), int(height * cls.size[1]), int(width * cls.position[0]), int(height * cls.position[1]))
